0ef7d533711fa2b98172f9ff208d02a4b516c6ad
Kankania <114629927+Kankania@users.noreply.github.com>
2022-09-29 11:16:23 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,17 @@
+# aplikasi biodata create kania 
+
+#memasukan nama
+print ("yuk lengkapi data diri anda..")
+
+#input data diri
+nama = input("nama      :")
+kelas = input("kelas    :")
+prodi = input("program studi  : ")
+
+print("Apakah data dibawah ini sudah benar ? ",nama)
+
+#print / Output Data Diri
+print("Nama anda adalah:", nama)
+print("Kelas anda adalah", kelas)
+
+    
