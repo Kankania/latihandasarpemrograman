cb7906caa17e3eeceb850c50f0f946d68ea72eb3
Kankania <114629927+Kankania@users.noreply.github.com>
2022-10-06 09:15:39 +0700

    Add files via upload

---
+++ Tugas3pemrograman.py
@@ -0,0 +1,7 @@
+# tugas sesi 3 by kania purnarahayu
+nama = "Kania Purnarahayu"
+kelas = "TI22A"
+jurusan = "Teknik Informatika"
+quote = "Perhisan terbaik seorang perempuan adalah sebuah ambisi"
+
+print(nama, kelas, jurusan, quote)
